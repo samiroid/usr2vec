ce5da475a3716a326ba40083b42d8ee4ec64384a
Ubuntu <ubuntu@ip-172-31-16-249.ec2.internal>
2017-01-27 02:21:07 +0000

    solved some issues with sqlite DB used to store the word/context-window log probabilities

--- scripts/context_probabilities.sh
+++ scripts/context_probabilities.sh
@@ -1,9 +1,11 @@
 #!/bin/bash -e
 # clear
 
-WORD_EMBEDDINGS_BIN="DATA/embeddings/bin/word2vec.pkl"
-DATA="raw_data/sample.txt"
-CTX_PROBS_DB="raw_data/twmh_ctxprobs.db"
+WORD_EMBEDDINGS_BIN="DATA/embeddings/bin/embs_emoji_2_400"
+#DATA="raw_data/sample.txt"
+DATA="raw_data/user_corpus.txt"
+# CTX_PROBS_DB="raw_data/twmh_ctxprobs.db"
+CTX_PROBS_DB="/home/ubuntu/twmh_ctxprobs.db"
 
 python code/ctx_window_probs.py -input $DATA \
 										-emb ${WORD_EMBEDDINGS_BIN} \
